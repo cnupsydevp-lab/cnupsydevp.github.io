cb70726bef86104bad3b5dfb8b37ebd06cd9b0c8
cnupsydevp-lab <cnu.psy.devp@gmail.com>
2026-05-18 22:30:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // 현재 페이지 URL을 확인하여 네비게이션 메뉴에 active 클래스 추가
-    const currentLocation = location.href;
+    // 현재 페이지 URL 확인
+    const currentLocation = window.location.pathname.split("/").pop();
     const menuItems = document.querySelectorAll('.nav-links a');
-    const menuLength = menuItems.length;
 
-    for (let i = 0; i < menuLength; i++) {
-        if (menuItems[i].href === currentLocation) {
-            menuItems[i].classList.add("active");
+    menuItems.forEach(item => {
+        const itemLocation = item.getAttribute('href');
+        // 현재 페이지와 메뉴 링크가 일치하면 active 클래스 추가
+        if (itemLocation === currentLocation || (currentLocation === "" && itemLocation === "index.html")) {
+            item.classList.add("active");
         }
-    }
+    });
 });
